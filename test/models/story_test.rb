e5bea7fe77308df1224b539dae042ca319f96669
James Kerr <jkerr838@gmail.com>
2020-05-27 20:45:43 -0700

    Wrote test to do window function query

--- test/models/story_test.rb
+++ test/models/story_test.rb
@@ -20,4 +20,26 @@ class StoryTest < ActiveSupport::TestCase
 
     assert story.destroyed?
   end
+
+  test "stories with max scores" do
+    stories(:one).samples << Sample.new(score: 1000)
+    stories(:one).samples << Sample.new(score: 5000)
+
+    subquery = <<-SQL
+      SELECT story_id, score, ROW_NUMBER() OVER (PARTITION BY story_id ORDER BY score DESC) as row
+      FROM samples
+    SQL
+
+    query = <<-SQL
+      SELECT title, score
+      FROM stories s
+      JOIN (#{subquery}) scores
+      ON s.id = scores.story_id AND row = 1
+      ORDER BY score DESC
+    SQL
+
+    results = Story.find_by_sql(query).pluck(:title, :score)
+
+    assert_equal results, [["Story One", 5000], ["Story Two", 252]]
+  end
 end
